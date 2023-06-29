e78b3704e3a7fdd8ddb00c14bf913acf20c689f1
Gina Cendales <lezzlee111@gmail.com>
2023-06-29 13:12:40 -0500

    first commit

---
+++ solidityapp/contract/test/TasksContract.js
@@ -0,0 +1,46 @@
+const TasksContract = artifacts.require("TasksContract")
+
+contract("TasksContract", () => {
+
+    before(async () => {
+        this.tasksContract = await TasksContract.deployed()
+    })
+
+    it('migrate deployed successfully', async () => {
+        const address = this.tasksContract.address
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, "");
+
+    })
+
+    it('get tasks list', async () => {
+        const tasksCounter = await this.tasksContract.taskCounter();
+        const task = await this.tasksContract.tasks(tasksCounter);
+
+        assert.equal(task.id.toNumber(), tasksCounter);
+        assert.equal(task.title, "mi primer tarea");
+        assert.equal(task.description, "descripcion de tarea");
+        assert.equal(task.done, false);
+        assert.equal(tasksCounter, 1);
+    })
+
+    it('task created successfully', async () => {
+        const result = await this.tasksContract.createTask("some task", "description who");
+        const taskEvent = result.logs[0].args;
+        const tasksCounter = await this.tasksContract.taskCounter()
+
+        assert.equal(tasksCounter, 2);
+    })
+
+    it('task toggle done', async () => {
+        const result = await this.tasksContract.toggleDone(1);
+        const taskEvent = result.logs[0].args;
+        const task = await this.tasksContract.tasks(1);
+
+        assert.equal(task.done, true);
+        assert.equal(taskEvent.done, true);
+        assert.equal(taskEvent.id, 1);
+    })
+})
